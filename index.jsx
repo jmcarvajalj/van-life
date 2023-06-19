d1c5512a7bd683b42465ad52662bc8d25bf90cd8
josemiguelcarvajal07 <62575167+josemiguelcarvajal07@users.noreply.github.com>
2023-06-19 17:50:57 -0500

    test 5

--- index.jsx
+++ index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
   RouterProvider,
-  createStaticRouter,
+  createBrowserRouter,
   createRoutesFromElements,
   Route
 } from "react-router-dom"
@@ -27,7 +27,7 @@ import { requireAuth } from "./utils"
 
 import "./server"
 
-const router = createStaticRouter(createRoutesFromElements(
+const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<Home />} />
     <Route path="about" element={<About />} />
